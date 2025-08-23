d9f66677c4c52a62234fb6a486315380e23b299d
Hasan Sayfi <hsayfi@jahez.net>
2025-08-23 07:58:21 +0300

    Minor fixes to imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,4 @@
 # Utilities package
 from .health_check import health_checker
-from .version import VERSION
-from .telegram_utils import *
-from .user_loader import *
 
-__all__ = ['health_checker', 'VERSION']
+__all__ = ['health_checker']
